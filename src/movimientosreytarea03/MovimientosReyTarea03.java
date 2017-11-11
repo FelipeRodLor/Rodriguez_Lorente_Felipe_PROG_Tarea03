5e66d457b4aca18c3f84503812f433a5c39b6d58
FelipeRodriguezLorente <>
2017-11-11 19:43:02 +0100

    Se añade la clase Entrada en el paquee "utilidades".

---
+++ src/movimientosreytarea03/MovimientosReyTarea03.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movimientosreytarea03;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class MovimientosReyTarea03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
