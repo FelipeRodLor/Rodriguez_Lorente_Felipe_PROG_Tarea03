b0ac0df6b4cfe864f0487833c4e32564d461f6ef
FelipeRodriguezLorente <Felipillo@xugaca-PC>
2017-11-13 19:03:00 +0100

    Se crean los metodos get y set para los atributos de la clase "Posicion".

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -32,4 +32,33 @@ public class Posicion {
             System.out.println("Columna incorrecta, se le ha asignado la columna a por defecto");
         }
     }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila <= 8 && fila >= 1) {
+            this.fila = fila;
+        } else {
+            this.fila = 1;
+            System.out.println("Fila incorrecta, se le ha asignado la fila 1 por defecto");
+
+        }
+    }
+
+    public char getColumna() {
+        return columna;
+
+    }
+
+    public void setColumna(char columna) {
+        if (columna == 'a' || columna == 'b' || columna == 'd' || columna == 'e' || columna == 'f' || columna == 'g' || columna == 'h') {
+            this.columna = columna;
+        } else {
+            this.columna = 'a';
+            System.out.println("Columna incorrecta, se le ha asignado la columna a por defecto");
+        }
+    }
+
 }
--- src/utilidades/Entrada.java
+++ src/utilidades/Entrada.java
@@ -122,5 +122,6 @@ public class Entrada {
             } while (!leido);
             return valor.charAt(0);
 	}
+       
 
 }
