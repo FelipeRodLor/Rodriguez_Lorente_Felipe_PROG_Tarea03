5aa6d4273e91a84a572af1cf3e2781603c916df9
FelipeRodriguezLorente <>
2017-11-13 16:27:56 +0100

    Se crea el paquete "tarea03" con la clase enumerada "Color".

---
+++ src/tarea03/Color.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ * Clase enumerada Color donde se definen los dos tipos de colores que puede 
+ * tener el rey.
+ * 
+ * @author Felipe Rodriguez
+ */
+public enum Color {
+    BLANCO, NEGRO
+    
+}
