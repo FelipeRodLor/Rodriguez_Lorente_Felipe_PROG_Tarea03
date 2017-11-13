2da008b0ec3c558531b71c6b606a5cd5d64cfdec
FelipeRodriguezLorente <>
2017-11-13 16:34:30 +0100

    Se crea la clase enumerada "Direccion" dentro del paquete "tarea03".

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *Se crea la clase enumerada "Direccion" donde se recoge las posibilidades de
+ * movimiento que tiene el rey. 
+ * 
+ * @author Felipillo
+ */
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE
+    
+}
