267a81049fd10382d841f8c5f64097f905539486
FelipeRodriguezLorente <>
2017-11-15 23:02:00 +0100

    Se crea la clase Principal con el metodo main. Se elimina el paquete "MovimientosReyTarea03" que no tenia ninguna utilidad.

--- src/tarea03/Principal.java
+++ src/tarea03/Principal.java
@@ -3,19 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package movimientosreytarea03;
+package tarea03;
 
 /**
  *
  * @author Felipillo
  */
-public class MovimientosReyTarea03 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Principal {
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
